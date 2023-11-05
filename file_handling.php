d27b1ca11e95ed0ec41140b422dfe96cbaacd53f
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2023-11-05 13:58:50 +0600

    Update file_handling.php

--- file_handling.php
+++ file_handling.php
@@ -1,1 +1,56 @@
+<?php
 
+// Task 1:
+
+// Write a PHP code to read the content of a text file and display it on the webpage.
+
+// Reading 'demo.txt'
+$file = 'demo.txt';
+
+
+// Open the file
+$fp = fopen($file,'r');
+
+
+// Read the content of the file
+$lines = [];
+while($line = fgets($fp)) {
+    array_push($lines, $line);
+}
+
+
+// Close the file
+fclose($fp);
+
+
+// Display the content
+foreach($lines as $line) {
+    echo $line."<br>";
+}
+
+
+
+// Task 2:
+
+// Write a PHP code to append new data to an existing text file.
+
+// Appending new lines to 'demo.txt'
+$file = 'demo.txt';
+
+$data = 'Writing this new data to the existing file'."\n";
+
+
+// Open the file in append mode
+$fp = fopen($file, 'a+');
+
+
+
+// Write the data to the file
+fwrite($fp, $data);
+
+
+
+// Close the file
+fclose($fp);
+
+?>
